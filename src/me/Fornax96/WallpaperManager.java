fc08ac344c22d23d918ae613b72d3abe3204cd17
Fornax96 <wimfris.tk@gmail.com>
2015-04-16 11:13:38 +0200

    Moved something

--- src/me/Fornax96/WallpaperManager.java
+++ src/me/Fornax96/WallpaperManager.java
@@ -3,15 +3,15 @@ package me.Fornax96;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /**
  * @author Fornax
  */
 public class WallpaperManager {
-
-	public void update(Double time) {
-		Integer number = time.intValue();
-		
+	private final ArrayList<String> backdrops = new ArrayList();
+	
+	public WallpaperManager(){
 		try {
 			ProcessBuilder pb = new ProcessBuilder(
 				"xfconf-query",
@@ -26,6 +26,22 @@ public class WallpaperManager {
 
 			String line = bri.readLine();
 			while(line != null) {
+				backdrops.add(line);
+				
+				line = bri.readLine();
+			}
+
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace(System.err);
+		}
+	}
+	
+
+	public void update(Double time) {
+		Integer number = time.intValue();
+		
+		try {
+			for(String line : backdrops) {
 				if(line.contains("image-path") || line.contains("last-image")){
 					new ProcessBuilder("xfconf-query",
 						"-c", "xfce4-desktop",
@@ -33,11 +49,9 @@ public class WallpaperManager {
 						"-s", ButtonMonitor.getBgLocation() + "bg-" + number + ".png")
 						.start();
 				}
-				
-				line = bri.readLine();
 			}
 
-		} catch (IOException | InterruptedException e) {
+		} catch (IOException e) {
 			e.printStackTrace(System.err);
 		}
 	}
